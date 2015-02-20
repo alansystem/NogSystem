41c10d38212c92c503dfc205b4548d31ae2dfee2
Alan Nogueira <alan.sistema@gmail.com>
2015-02-20 20:24:40 -0200

    Inserção de criptografia para sessões

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'jduy76dh7&j2$m3409*&d';
 
 /*
 |--------------------------------------------------------------------------
