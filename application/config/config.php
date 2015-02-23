c30b45cf300951054b8898aa9ad5d1e90132e1fa
Alan Nogueira <alan.sistema@gmail.com>
2015-02-22 22:30:01 -0300

    Configurações básicas após voltar a versão do codeigniter

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://localhost/nogystem';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '$#sf3432423osdf873457%#2*02n';
 
 /*
 |--------------------------------------------------------------------------
