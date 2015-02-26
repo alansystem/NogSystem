ea3aa03ad384e272c42963f106787132623b3093
Alan Nogueira <alan.sistema@gmail.com>
2015-02-25 22:03:31 -0300

    Alteração de Pastas para o Layout

---
+++ application/views/footer.php
@@ -0,0 +1,35 @@
+
+            </div>
+        </div>
+
+        <!-- footer -->
+        <div class="navbar navbar-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <footer role="contentinfo">
+                            <p class="left">NogSystem CI</p>
+                            <p class="right">&copy; 2013 <a href="http://www.meritoo.pl" target="_blank">Meritoo.pl</a></p>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script type="text/javascript" href="<?php echo base_url('assets/theme'); ?>/js/bootstrap.min.js"></script>
+        <script type="text/javascript" href="<?php echo base_url('assets/theme'); ?>/js/twitter-bootstrap-hover-dropdown.min.js"></script>
+        <script type="text/javascript" href="<?php echo base_url('assets/theme'); ?>/js/bootstrap-admin-theme-change-size.js"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/theme'); ?>/vendors/easypiechart/jquery.easy-pie-chart.js"></script>
+
+        <script type="text/javascript">
+            $(function() {
+                // Easy pie charts
+                $('.easyPieChart').easyPieChart({animate: 1000});
+            });
+        </script>
+    </body>
+</html>
